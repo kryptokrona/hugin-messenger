5fcdc10f84d161a4cfbb4de1d812a389151d532f
knugen <harry@Harrys-MacBook-Air.local>
2021-06-30 22:03:35 +0200

    Fixed issue with some links not opening correctly in external browser

--- renderer.js
+++ renderer.js
@@ -89,7 +89,7 @@ closeAppLogin.addEventListener('click', () => {
 let walletd;
 
 // Open links in browser
-$(document).on('click', 'a[href^="http"]', function(event) {
+$(document).on('click', 'a', function(event) {
     event.preventDefault();
     shell.openExternal(this.href);
 });
