0f30994a40b1ab20fe837f48396a37a77ec4f0ee
n9lsjr <rasmus@lingonbyran.se>
2021-12-07 18:40:27 +0100

    Remove border rgb when starting a call

--- messages.js
+++ messages.js
@@ -370,7 +370,7 @@ let startCall = (audio, video, screenshare=false) => {
 
   $('#messages_pane').find('audio').remove();
   $('#messages_pane').append('<audio autoplay><source src="static/startcall.mp3" type="audio/mpeg"></audio>');
-  $('#messages_contacts > li').css('border-left', 'none');
+  $('.active_contact').removeClass('border-rgb');
 
   // $('#video-button').unbind('click');
   //
